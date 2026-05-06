c65ea1fbaf9cdac2badd553263334313fec07d1f
dgando-bit <bak1tino@gmail.com>
2026-05-06 10:51:53 +0200

    :art: update logger import

--- ml/src/main.py
+++ ml/src/main.py
@@ -1,4 +1,4 @@
-from shared.shared.logger import get_logger
+from shared.logger import get_logger
 
 logger = get_logger(__name__)  # __name__ = nom du module automatiquement
 logger.info("Lancement du script...")
